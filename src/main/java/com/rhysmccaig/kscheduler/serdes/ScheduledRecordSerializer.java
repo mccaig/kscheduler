23c48a22b65f2655b36c32abb6c5abee8ce3c1c6
Rhys Anthony McCaig <mccaig@gmail.com>
2020-05-28 02:29:19 -0700

    .

--- src/main/java/com/rhysmccaig/kscheduler/serdes/ScheduledRecordSerializer.java
+++ src/main/java/com/rhysmccaig/kscheduler/serdes/ScheduledRecordSerializer.java
@@ -1,11 +1,11 @@
 package com.rhysmccaig.kscheduler.serdes;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.protobuf.ByteString;
 import com.rhysmccaig.kscheduler.model.ScheduledRecord;
 import com.rhysmccaig.kscheduler.model.protos.Protos;
-import com.rhysmccaig.kscheduler.model.protos.Protos.ScheduledRecordHeader;
 
 import org.apache.kafka.common.serialization.Serializer;
 
@@ -20,9 +20,12 @@ public class ScheduledRecordSerializer implements Serializer<ScheduledRecord> {
   }
 
   private static Protos.ScheduledRecord toProto(ScheduledRecord record) {
-    List<Protos.ScheduledRecordHeader> headers = (record.headers() == null) ? List.of() : List.of(record.headers()).stream()
+    List<Protos.ScheduledRecordHeader> headers = (record.headers() == null) ? List.of() : List.of(record.headers().toArray()).stream()
         .map(h -> Protos.ScheduledRecordHeader.newBuilder()
-            .setKey(h.k))
+            .setKey(h.key())
+            .setValue(ByteString.copyFrom(h.value()))
+            .build())
+        .collect(Collectors.toList());
     return Protos.ScheduledRecord.newBuilder()
         .setMetadata(ScheduledRecordMetadataSerializer.toProto(record.metadata()))
         .setKey(ByteString.copyFrom(record.key()))
