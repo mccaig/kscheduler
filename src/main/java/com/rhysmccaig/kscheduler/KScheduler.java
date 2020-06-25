c4811e7092dc1ba1970c48cf9b0c05578a9e5194
Rhys Anthony McCaig <mccaig@gmail.com>
2020-06-25 01:43:50 -0700

    .

--- src/main/java/com/rhysmccaig/kscheduler/KScheduler.java
+++ src/main/java/com/rhysmccaig/kscheduler/KScheduler.java
@@ -49,7 +49,7 @@ public class KScheduler {
     final Config topicsConfig = config.getConfig("topics");
     final Config streamsConfig = config.getConfig("kafka.streams");
 
-    final Duration streamsShutdownTimeout = streamsConfig.getDuration("shutdown.timeout");
+    final Duration streamsShutdownTimeout = schedulerConfig.getDuration("shutdown.timeout");
 
     Properties streamsProps = ConfigUtils.toProperties(streamsConfig);
     // Force these settings
